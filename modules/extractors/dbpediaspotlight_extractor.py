0c6980d9a6157991a92fe925816344af982bd820
Nikit Srivastava <nikit91@gmail.com>
2022-03-31 17:34:57 +0200

    updating knowledge extractor code

---
+++ modules/extractors/dbpediaspotlight_extractor.py
@@ -0,0 +1,7 @@
+def extract_resources(text_question, answer_list):
+    entity_list = list()
+    relation_list = list()
+    """
+    Knowledge Extraction logic here
+    """
+    return entity_list, relation_list
