d3c12a61c7b63e8beec04daba9f64cf46fd956f8
abdullahfathi <ahmed.f.abdullah@gmail.com>
2022-04-01 16:38:44 +0200

    falcon extractor

--- modules/extractors/falcon_extractor.py
+++ modules/extractors/falcon_extractor.py
@@ -1,8 +1,35 @@
-def extract_resources(text_question, answer_list):
-    q_entity_list = list()
-    q_relation_list = list()
-    a_entity_list = list()
-    """
-    Knowledge Extraction logic here
-    """
-    return q_entity_list, q_relation_list, a_entity_list
+import urllib3
+import json
+def extract_resources(question:str, answers:list):
+    result_ent_question, result_rel_question, result_ent_answer = [], [], []
+    http = urllib3.PoolManager()
+    data  = {"text":question}
+    encoded_data = json.dumps(data).encode('utf-8')
+    r = http.request(
+	'POST',
+	'https://labs.tib.eu/falcon/api?mode=long',
+	body=encoded_data,
+	headers={'Content-Type': 'application/json'}
+    )
+    question_out = json.loads(r.data.decode('utf-8'))
+    
+    for ans in answers:
+	data  = {"text":ans}
+	encoded_data = json.dumps(data).encode('utf-8')
+	r = http.request(
+	    'POST',
+	    'https://labs.tib.eu/falcon/api?mode=long',
+	    body=encoded_data,
+	    headers={'Content-Type': 'application/json'}
+	)
+	answer_out = json.loads(r.data.decode('utf-8'))
+	for ent in answer_out['entities']:
+	    result_ent_answer.append(ent[0])
+    
+    for ent in question_out['entities']:
+	result_ent_question.append(ent[0])
+	
+    for rel in question_out['relations']:
+	result_rel_question.append(rel[0])  
+	
+    return result_ent_question, result_rel_question, result_ent_answer
