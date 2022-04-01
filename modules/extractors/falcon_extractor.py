c460a1969e045b606d9d09a1450ae62f63ee207b
Jean-KOUAGOU <jeank@aims.ac.za>
2022-04-01 17:01:22 +0200

    fixed indentation problem

--- modules/extractors/falcon_extractor.py
+++ modules/extractors/falcon_extractor.py
@@ -6,30 +6,30 @@ def extract_resources(question:str, answers:list):
     data  = {"text":question}
     encoded_data = json.dumps(data).encode('utf-8')
     r = http.request(
-	'POST',
-	'https://labs.tib.eu/falcon/api?mode=long',
-	body=encoded_data,
-	headers={'Content-Type': 'application/json'}
+    'POST',
+    'https://labs.tib.eu/falcon/api?mode=long',
+    body=encoded_data,
+    headers={'Content-Type': 'application/json'}
     )
     question_out = json.loads(r.data.decode('utf-8'))
-    
+
     for ans in answers:
-	data  = {"text":ans}
-	encoded_data = json.dumps(data).encode('utf-8')
-	r = http.request(
-	    'POST',
-	    'https://labs.tib.eu/falcon/api?mode=long',
-	    body=encoded_data,
-	    headers={'Content-Type': 'application/json'}
-	)
-	answer_out = json.loads(r.data.decode('utf-8'))
-	for ent in answer_out['entities']:
-	    result_ent_answer.append(ent[0])
-    
+    data  = {"text":ans}
+    encoded_data = json.dumps(data).encode('utf-8')
+    r = http.request(
+        'POST',
+        'https://labs.tib.eu/falcon/api?mode=long',
+        body=encoded_data,
+        headers={'Content-Type': 'application/json'}
+    )
+    answer_out = json.loads(r.data.decode('utf-8'))
+    for ent in answer_out['entities']:
+        result_ent_answer.append(ent[0])
+
     for ent in question_out['entities']:
-	result_ent_question.append(ent[0])
-	
+    result_ent_question.append(ent[0])
+
     for rel in question_out['relations']:
-	result_rel_question.append(rel[0])  
-	
+    result_rel_question.append(rel[0])  
+
     return result_ent_question, result_rel_question, result_ent_answer
